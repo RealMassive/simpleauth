eb15ef2fb3f8431253207a7eaa6d504856cd367c
Eric Plaster <eric.plaster@realmassive.com>
2014-10-02 10:54:39 -0500

    Trying a larger version number

--- example/lib/simpleauth/__init__.py
+++ example/lib/simpleauth/__init__.py
@@ -3,7 +3,7 @@ A simple auth handler for Google App Engine supporting
 OAuth 1.0a, 2.0 and OpenID.
 """
 
-__version__ = '0.1.4-RM'
+__version__ = '0.1.5-RM'
 __license__ = "MIT"
 __author__ = "Alex Vagin (http://alex.cloudware.it)"
 
